3417b772f8f8e444a9a735bba5e63922f83ab318
Awk34 <koroluka@gmail.com>
2013-10-12 22:01:42 -0400

    made 5 better..

--- euler5.java
+++ euler5.java
@@ -3,20 +3,21 @@
 // Project Euler - 5    *
 //***********************
 
-public class euler5
-{
-	public static void main(String [] args)
-	{
+public class euler5 {
+	public static void main(String [] args) {
 		int num=1;
 
-		while(num<1000000000)
-		{
-			if(num%2==0 && num%3==0 && num%4==0 && num%5==0 && num%6==0 && num%7==0 && num%8==0 && num%9==0 && num%10==0 && num%11==0 && num%12==0 && num%13==0 && num%14==0 && num%15==0 && num%16==0 && num%17==0 && num%18==0 && num%19==0 && num%20==0)
-			{
-				System.out.println(num);
-				break;
+		while(num<1000000000) {
+			for(int i=2; i<=20; i++) {
+				if(num%i==0) {
+					continue;
+					if(i==20) {
+						System.out.println(num);
+						return;
+					}
+				}
+				else break;
 			}
-			num++;
 		}
 	}
 }

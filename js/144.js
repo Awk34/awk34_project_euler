78d857f748a15b9eda3d674302820dcbb08b9a98
Andrew Koroluk <koroluka@gmail.com>
2016-05-24 17:52:03 -0400

    feat(js/144): use *step, use exectimer

--- js/144.js
+++ js/144.js
@@ -1,7 +1,9 @@
 'use strict';
-
 import Euler from './Euler';
 import quadratic from './util/quadratic';
+import { decorate } from 'core-decorators';
+import t from 'exectimer';
+const Tick = t.Tick;
 
 class Point {
     constructor(x, y, z) {
@@ -12,6 +14,8 @@ class Point {
 }
 
 export default class Euler144 extends Euler {
+    p1;
+    p2;
     x1;
     y1;
     x2;
@@ -29,8 +33,15 @@ export default class Euler144 extends Euler {
         this.ans = 1;
     }
 
-    run() {
+    /**
+     * This is currently not supported by Babylon:
+     *  @decorate(Tick.wrap)
+     *  *step() {}
+     */
+    *step() {
         while(true) {
+            let tick = new Tick('step');
+            tick.start();
             let m0 = (this.y2 - this.y1) / (this.x2 - this.x1);
             let m1 = Euler144.deriv(this.x2, this.y2);
             let tempX = this.x2;
@@ -38,16 +49,56 @@ export default class Euler144 extends Euler {
             let X = (m0 - m1) / (1 + m0 * m1);
             let m2 = (m1 - X) / (1 + X * m1);
 
-            let b = (this.y2 - m2 * this.x2);
+            let b = this.y2 - m2 * this.x2;
             this.x2 = quadratic(m2, b, this.x2);
             this.y2 = m2 * this.x2 + b;
-            this.info(`${this.x2}, ${this.y2}`);
+            this.verbose(`Hit ${this.x2}, ${this.y2}`);
             this.x1 = tempX;
             this.y1 = tempY;
 
-            if(this.y2 > 0 && this.x2 > -.01 && this.x2 < .01) break;
-            this.ans++;
+            // new point is inside hole
+            if(this.y2 > 0 && this.x2 > -.01 && this.x2 < .01) {
+                tick.stop();
+                return {
+                    x1: this.x1,
+                    y1: this.y1,
+                    x2: this.x2,
+                    y2: this.y2,
+                    ans: this.ans
+                };
+            } else {
+                this.ans++;
+                tick.stop();
+                yield {
+                    x1: this.x1,
+                    y1: this.y1,
+                    x2: this.x2,
+                    y2: this.y2,
+                    ans: this.ans
+                };
+            }
         }
+    }
+
+    run() {
+        this.start();
+
+        const stepper = this.step();
+
+        let step;
+        do {
+            step = stepper.next();
+            this.verbose(`step: ${step.value}`);
+        } while(!step.done);
+
+        this.end();
+
+        let stepTimer = t.timers.step;
+
+        this.info(`Took ${stepTimer.count()} steps`);
+        this.info(`Took ${stepTimer.parse(stepTimer.duration())}`);
+        this.info(`Solution: ${this.ans}`);
+
         return this.ans;
     }
 
